565e6127a378a391431282a4ea639be7ee1d086e
Arun Bishop <arunleob@cmu.edu>
2026-07-20 13:03:33 -0400

    Add test for residual with softplus

--- julia/test/runtests.jl
+++ julia/test/runtests.jl
@@ -4,6 +4,8 @@
 # problem written directly as matrices (from julia/examples/simple_test.jl) and
 # the same 16 test cases in the same order — nine solve tests followed by seven
 # dimension validation cases.
+# using Pkg; Pkg.activate(joinpath(@__DIR__, "../examples/"))
+# using Revise
 using Test
 using LinearAlgebra
 using SparseArrays
@@ -144,5 +146,60 @@ retract(::Val{:ScaledExp}, x, κ) = sqrt(κ)*exp.(x./sqrt(κ))
         # Sparse blocks: L given but l omitted
         @test_throws ErrorException Marble.setup!(Marble.Solver(), sparse(Q2), zeros(2);
             L = sparse(zeros(1, 2)), R = sparse(zeros(1, 2)), r = zeros(1))
+    end
+
+    function kkt_residual(retract_type, solver, solution, κ, ρ)
+        # Extract solution, multipliers, and problem data
+        prob, workspace = solver.problem, solver.workspace
+        z, s_ineq, s_comp = solution[solver.z_inds], solution[solver.s_ineq_inds], solution[solver.s_comp_inds]
+        m_eq, m_ineq, m_comp_L, m_comp_R =
+            solution[solver.m_eq_inds], solution[solver.m_ineq_inds], solution[solver.m_comp_L_inds], solution[solver.m_comp_R_inds]
+        H, g, J_eq, c_eq, J_ineq, c_ineq, L_comp, l_comp, R_comp, r_comp = prob.cost_hessian, prob.cost_gradient, prob.J_eq, prob.c_eq, prob.J_ineq, prob.c_ineq, prob.L_comp, prob.l_comp, prob.R_comp, prob.r_comp
+        m_eq_est, m_ineq_est, m_comp_L_est, m_comp_R_est = workspace.m_eq_est, workspace.m_ineq_est, workspace.m_comp_L_est, workspace.m_comp_R_est
+
+        # Construct KKT residual
+        res = zeros(eltype(solution), length(solution))
+        # z stationarity
+        res[solver.z_inds] = H*z + g + J_eq'*m_eq + J_ineq'*m_ineq + L_comp'*m_comp_L + R_comp'*m_comp_R
+
+        # Inequality slack stationarity
+        p_neg_ineq = retract(retract_type, -s_ineq, κ)
+        dp_ineq = FD.jacobian(_x -> retract(retract_type, _x, κ), s_ineq)
+        res[solver.s_ineq_inds] = dp_ineq * (-m_ineq - p_neg_ineq)
+
+        # Complementarity slack stationarity
+        dp_comp = FD.jacobian(_x -> retract(retract_type, _x, κ), s_comp)
+        dp_neg_comp = FD.jacobian(_x -> retract(retract_type, _x, κ), -s_comp)
+        res[solver.s_comp_inds] = -dp_comp * m_comp_L + dp_neg_comp * m_comp_R
+
+        # Equality primal feasibility
+        res[solver.m_eq_inds] = J_eq*z + c_eq - 1/ρ*(m_eq - m_eq_est)
+
+        # Inequality primal feasibility
+        p_ineq = retract(retract_type, s_ineq, κ)
+        res[solver.m_ineq_inds] = J_ineq*z + c_ineq - p_ineq - 1/ρ*(m_ineq - m_ineq_est)
+
+        # Complementarity primal feasibility
+        p_comp_L = retract(retract_type, s_comp, κ)
+        p_comp_R = retract(retract_type, -s_comp, κ)
+        res[solver.m_comp_L_inds] = L_comp*z + l_comp - p_comp_L - 1/ρ*(m_comp_L - m_comp_L_est)
+        res[solver.m_comp_R_inds] = R_comp*z + r_comp - p_comp_R - 1/ρ*(m_comp_R - m_comp_R_est)
+        return res
+    end
+
+    @testset "KKT residual" begin
+        solver, _ = setup_and_solve()
+        prob, workspace = solver.problem, solver.workspace
+        res = workspace.kkt_residual
+
+        workspace.solution .= randn(length(workspace.solution))
+        ρ, κ = workspace.penalty_param, workspace.relax_param
+        Marble.update_residuals!(solver)
+        Marble.update_KKT_residual!(solver, κ, ρ)
+
+        retract_type = Val(:Softplus)
+        tol = 1e-12
+        res = kkt_residual(retract_type, solver, workspace.solution, κ, ρ)
+        isapprox(workspace.kkt_residual, res, atol=tol)
     end
 end
